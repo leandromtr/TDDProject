1ca3e0606810f81cdefbe9669fb62ac799da498e
leandromtr <leandromtr@hotmail.com>
2016-10-11 16:22:59 +0100

    Operations Test

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDProject
+{
+    public class Calculator
+    {
+        const int HoursInYear = 2080;
+
+        //Lambda Method
+        public int Add(int val1, int val2) => val1+val2;
+        public int Substract(int val1, int val2) => val1 - val2;
+        public int Multiply(int val1, int val2) => val1 * val2;
+        public int Divide(int val1, int val2) => val1 / val2;
+    }
+}
---
+++ TDDProject/CalculatorTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDDProject
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+
+        public void OperationAdd()
+        {
+            //Arrange - Organizar
+            Calculator obj = new Calculator();
+            //Act - Agir
+            int result = obj.Add(10, 10);
+            //Assert - Afirmar
+            Assert.AreEqual(20, result);
+        }
+        [TestMethod]
+        public void OperationSubstract()
+        {
+            Calculator obj = new Calculator();
+            int result = obj.Substract(10, 10);
+            Assert.AreEqual(0, result);
+        }
+        [TestMethod]
+        public void OperationMultiply()
+        {
+            Calculator obj = new Calculator();
+            int result = obj.Multiply(10, 10);
+            Assert.AreEqual(100, result);
+        }
+        [TestMethod]
+        public void OperationDivide()
+        {
+            Calculator obj = new Calculator();
+            int result = obj.Divide(10, 10);
+            Assert.AreEqual(1, result);
+        }
+    }
+}
